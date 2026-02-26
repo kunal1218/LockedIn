12bf31354eaeba11edd51ac112398d187aa75e9b
Kunal Singh <kunaldsingh26@gmail.com>
2026-02-25 20:52:02 -0600

    ye

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -7,7 +7,10 @@ const workspaceRoot = path.resolve(projectRoot, "../..");
 const config = getDefaultConfig(projectRoot);
 
 config.watchFolders = [workspaceRoot];
-config.resolver.nodeModulesPaths = [path.resolve(projectRoot, "node_modules")];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
 config.resolver.disableHierarchicalLookup = true;
 config.resolver.extraNodeModules = {
   react: path.resolve(projectRoot, "node_modules/react"),
